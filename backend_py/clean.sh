72d3509b486ec43838bf4761f38e06826b9afd13
brandonhs <brandonnms@gmail.com>
2026-05-05 10:55:16 -0700

    Prepare for v0.6.0 release

--- backend_py/clean.sh
+++ backend_py/clean.sh
@@ -6,6 +6,10 @@ find . -type f -name "*.so" -exec rm {} +
 # Find and remove all __pycache__ directories
 find . -type d -name "__pycache__" -exec rm -rf {} +
 
+find . -type d -name ".ruff_cache" -exec rm -rf {} +
+
+find . -type d -name ".pytest_cache" -exec rm -rf {} +
+
 # Remove the device_settings.json file if it exists
 rm -f device_settings.json || true
 rm -f server_preferences.json || true

1fa44ee44350c3778390f8ae4a15706c3829a322
brandonhs <brandonnms@gmail.com>
2026-05-22 15:21:50 -0700

    Update DeviceCard

--- frontend/src/components/dwe/cameras/device-card.tsx
+++ frontend/src/components/dwe/cameras/device-card.tsx
@@ -7,26 +7,32 @@ import {
 } from "@/components/ui/card";
 import { useDeviceStore } from "@/store/devices";
 import { CameraStream } from "./stream/stream";
+import { CameraNickname } from "./nickname";
 
 const DeviceCard = ({ bus_id }: { bus_id: string }) => {
-  const device = useDeviceStore((state) => state.devices[bus_id]);
+  const bus_info = useDeviceStore((state) => state.devices[bus_id].bus_info);
+  const deviceName = useDeviceStore((state) => state.devices[bus_id].name);
+  const deviceManufacturer = useDeviceStore(
+    (state) => state.devices[bus_id].manufacturer,
+  );
 
   return (
     <Card className="w-full max-w-sm mx-auto">
       <CardHeader>
         <div className="flex items-start justify-between gap-2">
           <div className="min-w-0 flex-1">
-            <CardTitle>{device.name}</CardTitle>
+            <CardTitle>{deviceName}</CardTitle>
             <CardDescription>
-              Manufacturer: {device.manufacturer}
+              Manufacturer: {deviceManufacturer}
               <br />
-              USB Port ID: {device.bus_info}
+              USB Port ID: {bus_info}
             </CardDescription>
           </div>
         </div>
+        <CameraNickname bus_id={bus_id} />
       </CardHeader>
       <CardContent>
-        <CameraStream bus_id={device.bus_info} />
+        <CameraStream bus_id={bus_info} />
       </CardContent>
     </Card>
   );
